233ac10782d340aeeca8b28ca55a22bc5de9c737
Rahul <yallarahul2004@gmail.com>
2024-08-14 20:23:24 +0530

    Initially

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  View,
+  Text
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{fontSize:30, color:'red'}}>Hello Hai ....</Text>
+    </View>
+  )
+}
+
+export default App
