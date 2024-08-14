0679843adc7227482dffa62962108fcf7455cece
Rahul <yallarahul2004@gmail.com>
2024-08-15 00:50:01 +0530

    StyleSheet in react-native

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
 import React from 'react';
-
 import {
-  //These are components, like tags in HTML
+  //These are called components, like tags in HTML
   View,
   Text,
   Button,
 } from 'react-native';
+import style1 from './Styles/style1';
 
 const App = () => {
   return (
     <View>
       <Text style={{fontSize: 30, color: 'red'}}>Hello Hai ....</Text>
+      <Text style={[style1.sty1,{color: 'red'}]}>Hello Hai ....</Text>
       <Button title="Submit" />
     </View>
   );
---
+++ Styles/style1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+
+const style1 = StyleSheet.create({
+    sty1 : {
+        color : 'blue',
+        fontSize : 40,
+        backgroundColor : 'yellow'
+    }
+})
+
+export default style1;
