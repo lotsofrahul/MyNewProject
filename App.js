51b2afc7816f5409ee36c96300a6604064602a90
Rahul <yallarahul2004@gmail.com>
2024-08-14 22:48:50 +0530

    Part1

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
 import React from 'react';
+
 import {
+  //These are components, like tags in HTML
   View,
-  Text
+  Text,
+  Button,
 } from 'react-native';
 
 const App = () => {
   return (
     <View>
-      <Text style={{fontSize:30, color:'red'}}>Hello Hai ....</Text>
+      <Text style={{fontSize: 30, color: 'red'}}>Hello Hai ....</Text>
+      <Button title="Submit" />
     </View>
-  )
-}
+  );
+};
 
-export default App
+export default App;
