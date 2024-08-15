c3d9286acfc3765eb754b7efbc3a53c9d3e28d8a
Rahul <yallarahul2004@gmail.com>
2024-08-15 12:32:22 +0530

    TextInput in react-native

--- App.js
+++ App.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   //These are called components, like tags in HTML
   View,
   Text,
   Button,
+  TextInput
 } from 'react-native';
 import style1 from './Styles/style1';
 
 const App = () => {
+  const [value, setValue] = useState("");
   return (
     <View>
       <Text style={{fontSize: 30, color: 'red'}}>Hello Hai ....</Text>
       <Text style={[style1.sty1,{color: 'red'}]}>Hello Hai ....</Text>
-      <Button title="Submit" />
+      <Button title={value} />
+      <TextInput style={{margin:10,fontSize:30,borderColor:'red',borderWidth:2}} 
+      placeholder='Enter something'
+      value={value}
+      onChangeText={(text)=>(setValue(text))}/>
+      <Button title='Clear Text' onPress={()=>{setValue("")}}/>
     </View>
   );
 };
